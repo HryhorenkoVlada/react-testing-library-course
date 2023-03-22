da2d31f743ef429aa691737f749b0615898df510
HryhorenkoVlada <vlada.hryhorenko@gotoinc.co>
2023-03-22 22:47:59 +0200

    feature: add ui for toppings option

--- src/pages/entry/ToppingOption.jsx
+++ src/pages/entry/ToppingOption.jsx
@@ -1,6 +1,9 @@
-import { Col } from 'react-bootstrap';
+import { Col, Form } from 'react-bootstrap';
 
-const ToppingOption = ({ name, imagePath }) => {
+const ToppingOption = ({ name, imagePath, updateItemCount }) => {
+  const handleChange = (e) => {
+    updateItemCount(name, e.target.checked ? 1 : 0);
+  };
   return (
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
@@ -8,6 +11,14 @@ const ToppingOption = ({ name, imagePath }) => {
         src={`http://localhost:3030${imagePath}`}
         alt={`${name} topping`}
       />
+      <Form.Group controlId={`${name}-topping-option`}>
+        <Form.Check
+          id={`${name}-topping-checkbox`}
+          type="checkbox"
+          label={name}
+          onChange={handleChange}
+        />
+      </Form.Group>
     </Col>
   );
 };
